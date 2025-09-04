9ac758759cfda4811c6e1150a935a7100a4c7d09
T3RK0-1101 <2021pjg13@gmail.com>
2025-09-04 07:20:49 -0600

    Agregar archivos de proyecto.

---
+++ 3NLIDTS-PabloJimenez-04c/Form1.Designer.cs
@@ -0,0 +1,293 @@
+﻿namespace _3NLIDTS_PabloJimenez_04c
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblnombre = new System.Windows.Forms.Label();
+            this.lblapellidos = new System.Windows.Forms.Label();
+            this.lbltelefono = new System.Windows.Forms.Label();
+            this.lblestatura = new System.Windows.Forms.Label();
+            this.lbledad = new System.Windows.Forms.Label();
+            this.tbnombre = new System.Windows.Forms.TextBox();
+            this.tbapellidos = new System.Windows.Forms.TextBox();
+            this.tbtelefono = new System.Windows.Forms.TextBox();
+            this.tbestatura = new System.Windows.Forms.TextBox();
+            this.tbedad = new System.Windows.Forms.TextBox();
+            this.rdbhombre = new System.Windows.Forms.RadioButton();
+            this.rdbmujer = new System.Windows.Forms.RadioButton();
+            this.btngenerar = new System.Windows.Forms.Button();
+            this.btncancelar = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblnombre
+            // 
+            this.lblnombre.AutoSize = true;
+            this.lblnombre.BackColor = System.Drawing.Color.Transparent;
+            this.lblnombre.Font = new System.Drawing.Font("Microsoft YaHei UI", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblnombre.Location = new System.Drawing.Point(124, 47);
+            this.lblnombre.Name = "lblnombre";
+            this.lblnombre.Size = new System.Drawing.Size(69, 19);
+            this.lblnombre.TabIndex = 0;
+            this.lblnombre.Text = "Nombre:";
+            // 
+            // lblapellidos
+            // 
+            this.lblapellidos.AutoSize = true;
+            this.lblapellidos.BackColor = System.Drawing.Color.Transparent;
+            this.lblapellidos.Font = new System.Drawing.Font("Microsoft YaHei UI", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblapellidos.Location = new System.Drawing.Point(117, 87);
+            this.lblapellidos.Name = "lblapellidos";
+            this.lblapellidos.Size = new System.Drawing.Size(76, 19);
+            this.lblapellidos.TabIndex = 1;
+            this.lblapellidos.Text = "Apellidos:";
+            // 
+            // lbltelefono
+            // 
+            this.lbltelefono.AutoSize = true;
+            this.lbltelefono.BackColor = System.Drawing.Color.Transparent;
+            this.lbltelefono.Font = new System.Drawing.Font("Microsoft YaHei UI", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbltelefono.Location = new System.Drawing.Point(121, 123);
+            this.lbltelefono.Name = "lbltelefono";
+            this.lbltelefono.Size = new System.Drawing.Size(72, 19);
+            this.lbltelefono.TabIndex = 2;
+            this.lbltelefono.Text = "Telefono:";
+            // 
+            // lblestatura
+            // 
+            this.lblestatura.AutoSize = true;
+            this.lblestatura.BackColor = System.Drawing.Color.Transparent;
+            this.lblestatura.Font = new System.Drawing.Font("Microsoft YaHei UI", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblestatura.Location = new System.Drawing.Point(127, 156);
+            this.lblestatura.Name = "lblestatura";
+            this.lblestatura.Size = new System.Drawing.Size(66, 19);
+            this.lblestatura.TabIndex = 3;
+            this.lblestatura.Text = "Estatura:";
+            // 
+            // lbledad
+            // 
+            this.lbledad.AutoSize = true;
+            this.lbledad.BackColor = System.Drawing.Color.Transparent;
+            this.lbledad.Font = new System.Drawing.Font("Microsoft YaHei UI", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbledad.Location = new System.Drawing.Point(147, 200);
+            this.lbledad.Name = "lbledad";
+            this.lbledad.Size = new System.Drawing.Size(46, 19);
+            this.lbledad.TabIndex = 4;
+            this.lbledad.Text = "Edad:";
+            // 
+            // tbnombre
+            // 
+            this.tbnombre.Location = new System.Drawing.Point(201, 46);
+            this.tbnombre.Name = "tbnombre";
+            this.tbnombre.Size = new System.Drawing.Size(100, 22);
+            this.tbnombre.TabIndex = 5;
+            // 
+            // tbapellidos
+            // 
+            this.tbapellidos.Location = new System.Drawing.Point(201, 84);
+            this.tbapellidos.Name = "tbapellidos";
+            this.tbapellidos.Size = new System.Drawing.Size(100, 22);
+            this.tbapellidos.TabIndex = 6;
+            // 
+            // tbtelefono
+            // 
+            this.tbtelefono.Location = new System.Drawing.Point(201, 120);
+            this.tbtelefono.Name = "tbtelefono";
+            this.tbtelefono.Size = new System.Drawing.Size(100, 22);
+            this.tbtelefono.TabIndex = 7;
+            // 
+            // tbestatura
+            // 
+            this.tbestatura.Location = new System.Drawing.Point(201, 155);
+            this.tbestatura.Name = "tbestatura";
+            this.tbestatura.Size = new System.Drawing.Size(100, 22);
+            this.tbestatura.TabIndex = 8;
+            // 
+            // tbedad
+            // 
+            this.tbedad.Location = new System.Drawing.Point(201, 197);
+            this.tbedad.Name = "tbedad";
+            this.tbedad.Size = new System.Drawing.Size(100, 22);
+            this.tbedad.TabIndex = 9;
+            // 
+            // rdbhombre
+            // 
+            this.rdbhombre.AutoSize = true;
+            this.rdbhombre.Location = new System.Drawing.Point(74, 21);
+            this.rdbhombre.Name = "rdbhombre";
+            this.rdbhombre.Size = new System.Drawing.Size(86, 23);
+            this.rdbhombre.TabIndex = 10;
+            this.rdbhombre.TabStop = true;
+            this.rdbhombre.Text = "Hombre";
+            this.rdbhombre.UseVisualStyleBackColor = true;
+            // 
+            // rdbmujer
+            // 
+            this.rdbmujer.AutoSize = true;
+            this.rdbmujer.Location = new System.Drawing.Point(157, 21);
+            this.rdbmujer.Name = "rdbmujer";
+            this.rdbmujer.Size = new System.Drawing.Size(69, 23);
+            this.rdbmujer.TabIndex = 11;
+            this.rdbmujer.TabStop = true;
+            this.rdbmujer.Text = "Mujer";
+            this.rdbmujer.UseVisualStyleBackColor = true;
+            // 
+            // btngenerar
+            // 
+            this.btngenerar.BackColor = System.Drawing.Color.OliveDrab;
+            this.btngenerar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btngenerar.Font = new System.Drawing.Font("Microsoft YaHei UI", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btngenerar.Location = new System.Drawing.Point(4, 47);
+            this.btngenerar.Name = "btngenerar";
+            this.btngenerar.Size = new System.Drawing.Size(114, 44);
+            this.btngenerar.TabIndex = 13;
+            this.btngenerar.Text = "Generar";
+            this.btngenerar.UseVisualStyleBackColor = false;
+            this.btngenerar.Click += new System.EventHandler(this.btngenerar_Click);
+            // 
+            // btncancelar
+            // 
+            this.btncancelar.BackColor = System.Drawing.Color.Firebrick;
+            this.btncancelar.Font = new System.Drawing.Font("Microsoft YaHei UI", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncancelar.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btncancelar.Location = new System.Drawing.Point(4, 108);
+            this.btncancelar.Name = "btncancelar";
+            this.btncancelar.Size = new System.Drawing.Size(114, 44);
+            this.btncancelar.TabIndex = 14;
+            this.btncancelar.Text = "Borrar";
+            this.btncancelar.UseVisualStyleBackColor = false;
+            this.btncancelar.Click += new System.EventHandler(this.btncancelar_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.rdbmujer);
+            this.groupBox1.Controls.Add(this.rdbhombre);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft YaHei UI", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(121, 236);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(233, 60);
+            this.groupBox1.TabIndex = 15;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Genero";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DarkGoldenrod;
+            this.panel1.Location = new System.Drawing.Point(-14, 361);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(457, 25);
+            this.panel1.TabIndex = 16;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.MidnightBlue;
+            this.panel2.Location = new System.Drawing.Point(0, 358);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(439, 13);
+            this.panel2.TabIndex = 17;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.DarkGoldenrod;
+            this.panel3.Location = new System.Drawing.Point(0, -3);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(436, 27);
+            this.panel3.TabIndex = 17;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.MidnightBlue;
+            this.panel4.Location = new System.Drawing.Point(0, 12);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(439, 15);
+            this.panel4.TabIndex = 18;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(433, 383);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btncancelar);
+            this.Controls.Add(this.btngenerar);
+            this.Controls.Add(this.tbedad);
+            this.Controls.Add(this.tbestatura);
+            this.Controls.Add(this.tbtelefono);
+            this.Controls.Add(this.tbapellidos);
+            this.Controls.Add(this.tbnombre);
+            this.Controls.Add(this.lbledad);
+            this.Controls.Add(this.lblestatura);
+            this.Controls.Add(this.lbltelefono);
+            this.Controls.Add(this.lblapellidos);
+            this.Controls.Add(this.lblnombre);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblnombre;
+        private System.Windows.Forms.Label lblapellidos;
+        private System.Windows.Forms.Label lbltelefono;
+        private System.Windows.Forms.Label lblestatura;
+        private System.Windows.Forms.Label lbledad;
+        private System.Windows.Forms.TextBox tbnombre;
+        private System.Windows.Forms.TextBox tbapellidos;
+        private System.Windows.Forms.TextBox tbtelefono;
+        private System.Windows.Forms.TextBox tbestatura;
+        private System.Windows.Forms.TextBox tbedad;
+        private System.Windows.Forms.RadioButton rdbhombre;
+        private System.Windows.Forms.RadioButton rdbmujer;
+        private System.Windows.Forms.Button btngenerar;
+        private System.Windows.Forms.Button btncancelar;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+    }
+}
+
---
+++ 3NLIDTS-PabloJimenez-04c/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3NLIDTS_PabloJimenez_04c
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btncancelar_Click(object sender, EventArgs e)
+        {
+            tbnombre.Clear();
+            tbapellidos.Clear();   
+            tbedad.Clear();
+            tbestatura.Clear();
+            tbtelefono.Clear();
+            rdbhombre.Checked = false;
+            rdbmujer.Checked = false;
+        }
+
+        private void btngenerar_Click(object sender, EventArgs e)
+        {
+            string nombre = tbnombre.Text;
+            string apellidos = tbapellidos.Text;
+            string edad = tbedad.Text;
+            string estatura = tbestatura.Text;
+            string telefono = tbtelefono.Text;
+            string genero = "";
+            if (rdbhombre.Checked)
+            {
+                genero = "masculino";
+            }
+            else if (rdbmujer.Checked) 
+            {
+                genero = "femenino";
+            }
+            string datos = $"Nombre: {nombre}\r\nApellidos: {apellidos}\r\n" +
+               $"Edad: {edad}\r\nEstatura: {estatura}\r\n" +
+               $"Tel: {telefono}\r\nGenero: {genero}";
+            MessageBox.Show(datos, "Valores registrados",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+    }
+}
